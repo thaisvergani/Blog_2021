badde9cff0df1f0cd8e4601ce8ee4266ef446cb5
Thais Vergani <thais.vergani@rdstation.com>
2021-07-28 22:21:22 -0300

    Mostrar posts na listagem

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Post
 """
 request = o que entra (o que o navegador envia)
 response = response (o que nós retornamos para o navegador)
 """
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+
+    return render(
+        request, 
+        'blog/post_list.html',
+        {'post_list': posts}
+    )
